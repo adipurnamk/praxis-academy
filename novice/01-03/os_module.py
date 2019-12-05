c7208db81f9cd5e925ab2389fce438c465e0b646
adipurnamk <muhammad.adipurna.k@gmail.com>
2019-12-05 14:59:25 +0700

    stdlib

---
+++ novice/01-03/os_module.py
@@ -0,0 +1,5 @@
+import os
+os.getcwd()      # Return the current working directory
+
+os.chdir('/server/accesslogs')   # Change current working directory
+os.system('mkdir today')   # Run the command mkdir in the system shel
